7a4875657858c87e8d1c3d80f5a041932c49e0c8
Danilo Barbosa <101227933+DanhBarbosa@users.noreply.github.com>
2023-10-25 22:04:17 -0300

    dashboard-1

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -3,12 +3,11 @@ function calcularEstatisticas(){
     const tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
     const pendentes = tarefas.filter(t => t.concluida == false)
     const itensPendente = pendentes.length
-    document.querySelector("#itens-pendente").innerHTML = itensPendente
-
+    document.querySelector("#cardapio-pendente").innerHTML = itensPendente
 
     const concluidas = tarefas.filter(t => t.concluida == true)
     const itensAceitos = concluidas.length
-    document.querySelector("#itens-concluida").innerHTML = itensAceitos
+    document.querySelector("#cardapio-aceito").innerHTML = itensAceitos
 
     const valorTotalCardapio = tarefas.reduce((total, t) => total += +t.pontos, 0)
     document.querySelector("#total-cardapio").innerHTML = valorTotalCardapio
